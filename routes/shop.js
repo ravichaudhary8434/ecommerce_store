32eaa8f897ccdfa748a2d0daaef9fe6b81d0abdf
Ravi Chaudhary <malhotraravi842@gmail.com>
2022-06-28 10:57:11 +0530

    worked with handlebars templating engine

--- routes/shop.js
+++ routes/shop.js
@@ -1,15 +1,18 @@
-const path = require("path");
-
 const express = require("express");
-
-const rootDir = require("../util/path");
 
 const adminData = require("./admin");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.render("shop", { prods: adminData.products, docTitle: "Shop" });
+  res.render("shop", {
+    prods: adminData.products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: adminData.products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
 });
 
 module.exports = router;
