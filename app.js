e8d3894387939dfa6e195703f78d507729db52c1
Ravi Chaudhary <malhotraravi842@gmail.com>
2022-12-17 20:40:36 +0530

    added production ready packages

--- app.js
+++ app.js
@@ -9,8 +9,12 @@ const csrf = require("csurf");
 const flash = require("connect-flash");
 const throwErr = require("./util/throwErr");
 const multer = require("multer");
+const helmet = require("helmet");
+const compression = require("compression");
+const morgan = require("morgan");
+const fs = require("fs");
 
-const MONGODB_URI = `mongodb+srv://malhotraravi842:${process.env.MONGODB_PASS}@cluster0.iyaxi.mongodb.net/shop?retryWrites=true&w=majority`;
+const MONGODB_URI = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.iyaxi.mongodb.net/shop?retryWrites=true&w=majority`;
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -50,6 +54,15 @@ app.set("views", "views");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const authRoutes = require("./routes/auth");
+
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "access.log"),
+  { flags: "a" }
+);
+
+app.use(helmet());
+app.use(compression());
+app.use(morgan("combined", { stream: accessLogStream }));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
@@ -116,6 +129,6 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(MONGODB_URI)
   .then((res) => {
-    app.listen(3000);
+    app.listen(process.env.PORT);
   })
   .catch((err) => console.log(err));
